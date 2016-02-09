de33f1be4b483c37d9cf055df565c5a5bd33af6d
steve-gray <steve@eventualconsistency.net>
2016-02-09 20:16:41 +1100

    Addition three different ways.

---
+++ src/implementation/addition.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const Error = require('../definitions/error');
+
+module.exports = {
+  /**
+   * Add numbers together from simple, flat path variables.
+   */
+  addNumbers: function(x, y, responder) {
+    if (x <= 0 || y <= 0) {
+      responder.error({ message: 'Arguments must be positive integers' });
+      return;
+    }
+    responder.success(x + y);
+  },
+
+  /**
+   * Add numbers together using a complex structure input
+   */
+  addNumbersStructured: function(params, responder) {
+    if (params.leftOperand <= 0 || params.rightOperand <= 0) {
+      responder.error({ message: 'Arguments must be positive integers' });
+      return;
+    }
+
+    responder.success(params.leftOperand + params.rightOperand);
+  },
+
+  /**
+   * Add numbers together using an array of structures as input
+   */
+  addNumbersBulk: function(inputs, responder) {
+    const results = [];
+    for (const item of inputs.value) {
+      if (item.leftOperand <= 0 || item.rightOperand <= 0) {
+        responder.error({ message: 'Arguments must be positive integers' });
+        return;
+      }
+
+      results.push(item.leftOperand + item.rightOperand);
+    }
+    responder.success(results);
+  }
+}
